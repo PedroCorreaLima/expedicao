9f09e45b00101362c2d235c3c81b6c22828cc903
Pedro Lima <pedrohenriquecorrealima@yahoo.com>
2025-06-10 09:05:55 -0300

    Fix route redirection to ensure users are directed to the '/embalagem' page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\PedidoController;
-
+Route::redirect('/', '/embalagem');
 Route::get('/embalagem', [PedidoController::class, 'index'])->name('embalagem.index');
 Route::post('/embalagem/start/{id}', [PedidoController::class, 'start'])->name('embalagem.start');
 Route::post('/embalagem/stop/{id}', [PedidoController::class, 'stop'])->name('embalagem.stop');
