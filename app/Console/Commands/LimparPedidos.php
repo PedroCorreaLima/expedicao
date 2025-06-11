fb41b979ec171244d603b9b2258bd338cdaba02b
Pedro Lima <pedrohenriquecorrealima@yahoo.com>
2025-06-11 11:21:46 -0300

    Add command to truncate 'pedidos' table and clear all records

---
+++ app/Console/Commands/LimparPedidos.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class LimparPedidos extends Command
+{
+    protected $signature = 'limpar:pedidos';
+    protected $description = 'Remove todos os registros da tabela pedidos';
+
+    public function handle()
+    {
+        DB::table('pedidos')->truncate();
+        $this->info('Tabela pedidos limpa com sucesso.');
+    }
+}
